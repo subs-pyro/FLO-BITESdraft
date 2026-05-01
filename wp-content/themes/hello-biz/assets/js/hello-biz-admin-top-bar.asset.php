4a5c3ef1e3d7c29aea3a77963acec8f485202119
Pallavi-kampalli <kampallipallavi@gmail.com>
2026-05-01 19:19:04 +0530

    Final fix: remove WordPress core completely

--- wp-content/themes/hello-biz/assets/js/hello-biz-admin-top-bar.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => '090aacd2dd12f691abf0');
--- wp-content/themes/hello-biz/assets/js/hello-biz-admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => '26e486a0a346ad61fd0e');
